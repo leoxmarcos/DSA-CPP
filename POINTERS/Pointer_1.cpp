be049ef06a2007131c7ac827e0ae4cea3d848510
Parth Singh <parthsingh1947@gmail.com>
2026-07-21 12:46:53 +0530

    21 july

---
+++ POINTERS/Pointer_1.cpp
@@ -0,0 +1,37 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a=10;
+
+//     cout<<&a<<endl;
+//     int *ptr = &a;
+//     cout<<sizeof(ptr)<<endl;
+//     cout<<*ptr<<endl;
+
+//     float m=2.6;
+//     float *ptr1=&m;
+//     cout<<sizeof(ptr1)<<endl;
+
+// }
+
+
+
+ #include<iostream>
+ using namespace std;
+
+ int main(){
+
+    int a=10;
+    //Print address of a
+    int *ptr =&a;
+    cout << ptr << endl; // address of a
+    cout<<*ptr<<endl; // value inside a
+     int b=20;
+     ptr=&b;
+     cout<<ptr<<endl;
+     cout<<*ptr<<endl;
+     b=30;
+     cout<<*ptr<<endl;
+ }
